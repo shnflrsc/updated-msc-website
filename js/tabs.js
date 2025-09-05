446466185b97045af90e59f12d6ee66abaa39ea8
Aaron John Bitara <118180256+eyron29@users.noreply.github.com>
2025-09-05 19:50:12 +0800

    Fix backlogs

--- js/tabs.js
+++ js/tabs.js
@@ -1,13 +1,25 @@
 $(document).ready(function () {
   $('.tab').click(function () {
     const selectedTab = $(this).data('tab');
-
     $('.tab').removeClass('text-[#b9da05] border-b-2 border-[#b9da05] active-tab');
+    $('.tab').addClass('text-gray-200'); 
 
-    $(this).addClass('text-[#b9da05] border-b-2 border-[#b9da05] active-tab');
+    $(this).addClass('text-[#b9da05] border-b-2 border-[#b9da05]');
+    $(this).removeClass('text-gray-200');
+    $(this).addClass('active-tab'); 
 
     $('.tab-content').addClass('hidden');
-
     $('#tab-' + selectedTab).removeClass('hidden');
   });
+
+  const initialActiveTab = $('.tab.active-tab').data('tab');
+  if (initialActiveTab) {
+      $('#tab-' + initialActiveTab).removeClass('hidden');
+  } else {
+      const firstTab = $('.tab').first();
+      firstTab.addClass('text-[#b9da05] border-b-2 border-[#b9da05] active-tab');
+      firstTab.removeClass('text-gray-200');
+      const firstTabName = firstTab.data('tab');
+      $('#tab-' + firstTabName).removeClass('hidden');
+  }
 });
